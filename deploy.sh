b6bbd5b7339d4bb79f9500ad91529e43cc8f1f1d
Linsama <orange24k@hotmail.com>
2020-03-22 00:17:30 +0800

    update project at 2020年03月22日  0:17:30

--- deploy.sh
+++ deploy.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
+workspace=`pwd`
+
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
 hugo
 
-# Go To Public folder
-cd public
+# Go To content folder
+cd ${workspace}/docs
 # Add changes to git.
-git add .
 
 # Commit changes.
-msg="rebuilding site `date`"
+msg="update website at `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
-git commit -m "$msg"
+git commit -am "$msg"
 
 # Push source and build repos.
 git push origin master
 
 # Come Back up to the Project Root
-cd ..
+cd ${workspace}
 
 echo -e "\033[0;32mstart to sync to blog repo...\033[0m"
-git add .
-git commit -m "sync project `date`"
+git commit -am "update project at `date`"
 git push origin master
 
